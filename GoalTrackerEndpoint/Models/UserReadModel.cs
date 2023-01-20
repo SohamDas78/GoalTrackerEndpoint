b89fd6efc679b2ca32deeb306700db0b7ab47875
Soham Das <soham.das78@live.com>
2023-01-20 22:53:43 +0530

    Added methods to add user in POST mode

---
+++ GoalTrackerEndpoint/Models/UserReadModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+namespace GoalTrackerEndpoint.Models
+{
+    public class UserReadModel
+    {
+        public UserReadModel(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
